3d1724b0aa652039559193b00bad7bdb5985fbf8
macbook <rohmat661@gmail.com>
2019-12-11 16:50:43 +0700

    udah lah capek

--- framework RN/android/app/src/main/java/com/erabangunjaya/android/MainApplication.java
+++ framework RN/android/app/src/main/java/com/erabangunjaya/android/MainApplication.java
@@ -10,7 +10,6 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
